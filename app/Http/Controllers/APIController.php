e5ad60e95c6f12048f116147713bbe41cc914a4f
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2021-10-27 02:22:13 +0600

    API for featured blog has written.

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Blog;
 
 class APIController extends Controller
 {
-    //
+    public function latestBlog(){
+      $latestBlog=Blog::where('publication_status', 1)
+            ->orderBy('id', 'DESC')->take(1)->get();
+
+      return $latestBlog;
+    }
 }
